f8343eb948339600cdcb534a4c372c228a96cfa6
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2025-05-13 06:10:57 -0400

    Prometheus Server for Go

---
+++ Prometheus/Server/README.md
@@ -0,0 +1,11 @@
+# Prometheus Http Request Counter
+
+- [Reference](https://prometheus.io/docs/tutorials/instrumenting_http_server_in_go/)
+
+## Example Output
+
+- Server Metrics
+![alt text](image.png)
+- [Prometheus Metrics](http://localhost:9000/graph?g0.expr=ping_requests_total&g0.tab=0&g0.display_mode=lines&g0.show_exemplars=0&g0.range_input=1h)
+
+![alt text](image-1.png)
--- Prometheus/Server/main.go
+++ Prometheus/Server/main.go
@@ -4,16 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var pingCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "ping_requests_total",
+		Help: "Total number of ping requests handled",
+	},
+)
+	
 
 func ping(w http.ResponseWriter, req *http.Request) {
 	log.Println("Got ping! Sending pong!")
+	pingCounter.Inc()
 	fmt.Fprintf(w, "pong")
 }
 
 func main() {
+	prometheus.MustRegister(pingCounter)
+
 
 	http.HandleFunc("/ping", ping)
+	http.Handle("/metrics", promhttp.Handler())
+	
 	log.Println("Starting on 8090, /ping is available")
 	http.ListenAndServe(":8090", nil)
 }
